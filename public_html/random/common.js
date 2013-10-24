d133edbd380733184711dca41168e86ec1410c33
comingshota <comingshota@yahoo.co.jp>
2013-10-24 13:37:45 +0900

    スタイル修正

--- public_html/random/common.js
+++ public_html/random/common.js
@@ -1,5 +1,5 @@
 $(function () {
-    $("input[type='text']").keypress(function(ev) {
+    $("input[type='text']").keypress(function (ev) {
         if ((ev.which && ev.which === 13) || (ev.keyCode && ev.keyCode === 13)) {
             return false;
         } else {
